e23bb84cbed51c820e479da2e262ad1d6ef93048
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-06-18 19:41:04 +0300

    Second commit

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,3 @@
 from django.urls import path
-from .views import success, cancel
 
-urlpatterns = [
-    path("cancel/", cancel, name="cancel"),
-    path("success/", success, name="success"),
-]
+urlpatterns = []
