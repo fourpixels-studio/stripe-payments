a5f9427cc874ca4926c3fd46ee43f84efbc69c2f
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-06-18 19:37:54 +0300

    Second commit

--- products/models.py
+++ products/models.py
@@ -5,13 +5,11 @@ from django.urls import reverse
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
+    file = models.FileField(upload_to="product_files/", blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-
-    @property
-    def get_display_price(self):
-        return "{0:.2f}".format(self.price / 100)
 
     @property
     def get_url(self):
